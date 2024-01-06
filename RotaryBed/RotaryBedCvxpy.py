8d3a42b2bc7394f97a81b617d8c05706a556aa27
jialuw96 <jwang44@nd.edu>
2024-01-05 19:08:15 -0500

    Comment cvxpy code; rename

--- RotaryBed/RotaryBedCvxpy.py
+++ RotaryBed/RotaryBedCvxpy.py
@@ -5,11 +5,16 @@ import pandas as pd
 import cvxpy as cp
 from measure_optimize import MeasurementOptimizer
 
-
+# This data file contains the sensitivity of measurements w.r.t. to each parameter 
+# column: parameter names 
+# row: measurements, stacked one by one, each measurement has 5 time points 
 jaco_info = pd.read_csv('./RotaryBed/Q5.csv', index_col=False)
 
 jaco_list = np.asarray(jaco_info)
 
+# stack jacobian as: 
+# rows: measurements stack one by one, each measurement has 5 time points
+# columns: 5 parameters 
 jaco = []
 for i in range(len(jaco_list)):
     jaco.append(list(jaco_list[i][1:]))
@@ -28,4 +33,4 @@ calculator = MeasurementOptimizer(jaco, number_measure, number_t, cost, verbose=
 
 calculator.compute_FIM(solution1)
 
-calculator.continuous_optimization(objective='D', budget=20)
+calculator.continuous_optimization_cvxpy(objective='D', budget=20)
